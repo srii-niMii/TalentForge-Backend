e0d3e515472fa38becc37644ca56041b48bf68e0
srii-niMii <nimisri198@gmail.com>
2026-08-02 02:10:22 +0530

    Integrate Cloudinary for resume storage

--- src/main/java/TalentForge/service/FileStorageService.java
+++ src/main/java/TalentForge/service/FileStorageService.java
@@ -1,57 +1,40 @@
 package TalentForge.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
-import java.nio.file.*;
+import java.util.Map;
 
 @Service
 public class FileStorageService {
 
 
-    private final Path uploadLocation =
-            Paths.get("uploads/resumes");
-
-
-    public FileStorageService() {
-
-        try {
-
-            Files.createDirectories(uploadLocation);
-
-        } catch (IOException e) {
-
-            throw new RuntimeException(
-                    "Could not create upload folder"
-            );
-        }
+    private final Cloudinary cloudinary;
+    public FileStorageService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
     }
 
     public String saveFile(MultipartFile file) {
-
-
         try {
 
-            String fileName =
-                    System.currentTimeMillis()
-                            + "_"
-                            + file.getOriginalFilename();
+            Map uploadResult =
+                    cloudinary.uploader()
+                            .upload(
+                                    file.getBytes(),
+                                    ObjectUtils.asMap(
+                                            "resource_type",
+                                            "raw",
+                                            "folder",
+                                            "talentforge/resumes"
+                                    )
+                            );
 
 
-            Path filePath =
-                    uploadLocation.resolve(fileName);
-
-
-
-            Files.copy(
-                    file.getInputStream(),
-                    filePath,
-                    StandardCopyOption.REPLACE_EXISTING
-            );
-
-
-            return filePath.toString();
+            return uploadResult
+                    .get("secure_url")
+                    .toString();
 
 
         } catch (IOException e) {
